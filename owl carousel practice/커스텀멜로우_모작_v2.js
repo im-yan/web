faa8a2b669ae4ee9a3074229b829595abcb29245
im-yan <click.me.yan@gmail.com>
2019-10-21 13:49:10 +0900

    커스텀멜로우 슬라이드쇼 모작

---
+++ owl carousel practice/커스텀멜로우_모작_v2.js
@@ -0,0 +1,14 @@
+$('.customellowV2 > .owl-carousel').owlCarousel({
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    loop:true,
+    nav:true,
+    navText:['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+    margin:0,
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+})
---
+++ owl carousel practice/커스템멜로우_모작_v1.js
@@ -0,0 +1,14 @@
+$('.customellow > .owl-carousel').owlCarousel({
+    autoplay:true,
+    loop:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    margin:0,
+    nav:true,
+    navText:['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+})
